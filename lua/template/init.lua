0d90b4a8f77c50c9ecab987c367dd9204002ac0f
Chifeng Wang <chifi@Chifengs-MacBook-Pro.local>
2024-12-20 03:34:37 -0500

    fix selection

--- lua/template/init.lua
+++ lua/template/init.lua
@@ -127,7 +127,7 @@ function temp.get_temp_list()
             goto continue
         end
         local extension = Get_file_extention(name)
-        if vim.filetype.match({ filename = name }) ~= nil or current_ext == extension then
+        if vim.filetype.match({ filename = name }) == current_ext or current_ext == extension then
             if not res[current_ext] then
                 res[current_ext] = {}
             end
