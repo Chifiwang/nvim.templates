0dbd587412cd4c8e9a087c9d93d6fd02e54b2a06
Chifeng Wang <chifi@Chifengs-MacBook-Pro.local>
2024-12-20 01:04:15 -0500

    edit

--- lua/template/init.lua
+++ lua/template/init.lua
@@ -83,7 +83,7 @@ renderer.render_line = function(line)
 end
 
 function Get_file_extention(url)
-  return url:match("^.+%.(.+)$")
+  return url:match("^.+\\.(.+)$")
 end
 
 function temp.get_temp_list()
